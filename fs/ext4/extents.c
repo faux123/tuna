4efd86f9d55a76e3f6ddc7c1c3b2124ed7018dee
Robin Dong <sanbai@taobao.com>
2011-07-11 11:43:59 -0400

    ext4: remove redundant goto in ext4_ext_insert_extent()
    If eh->eh_entries is smaller than eh->eh_max, the routine will
    go to the "repeat" and then go to "has_space" directlly ,
    since argument "depth" and "eh" are not even changed.
    
    Therefore, goto "has_space" directly and remove redundant "repeat" tag.
    
    Signed-off-by: Robin Dong <sanbai@taobao.com>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -1722,7 +1722,6 @@ int ext4_ext_insert_extent(handle_t *handle, struct inode *inode,
 		goto merge;
 	}
 
-repeat:
 	depth = ext_depth(inode);
 	eh = path[depth].p_hdr;
 	if (le16_to_cpu(eh->eh_entries) < le16_to_cpu(eh->eh_max))
@@ -1744,7 +1743,7 @@ repeat:
 			ext_debug("next leaf isn't full(%d)\n",
 				  le16_to_cpu(eh->eh_entries));
 			path = npath;
-			goto repeat;
+			goto has_space;
 		}
 		ext_debug("next leaf has no free space(%d,%d)\n",
 			  le16_to_cpu(eh->eh_entries), le16_to_cpu(eh->eh_max));
