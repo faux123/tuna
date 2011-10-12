175e03f0d341aea431db57a548c477d8da63059b
Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
2011-10-12 16:17:54 -0700

    jump-label: initialize jump-label subsystem much earlier
    Initialize jump_labels much, much earlier, so they're available for use
    during system setup.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>

--- kernel/jump_label.c
+++ kernel/jump_label.c
@@ -134,7 +134,7 @@ static void __jump_label_update(struct jump_label_key *key,
 	}
 }
 
-static __init int jump_label_init(void)
+void __init jump_label_init(void)
 {
 	struct jump_entry *iter_start = __start___jump_table;
 	struct jump_entry *iter_stop = __stop___jump_table;
@@ -160,10 +160,7 @@ static __init int jump_label_init(void)
 #endif
 	}
 	jump_label_unlock();
-
-	return 0;
 }
-early_initcall(jump_label_init);
 
 #ifdef CONFIG_MODULES
 
