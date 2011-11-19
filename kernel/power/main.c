5fcfc20e6c5c25ea9025d8aba7ca8d7a027cf180
Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
2011-11-19 06:19:12 -0800

    PM / Suspend: Fix bug in suspend statistics update
    A pair of missing braces inside the state_store() function causes even
    invalid arguments to suspend to be wrongly treated as failed suspend
    attempts. Fix this.
    
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -195,7 +195,7 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 		if (*s && len == strlen(*s) && !strncmp(buf, *s, len))
 			break;
 	}
-	if (state < PM_SUSPEND_MAX && *s)
+	if (state < PM_SUSPEND_MAX && *s) {
 #ifdef CONFIG_EARLYSUSPEND
 		if (state == PM_SUSPEND_ON || valid_state(state)) {
 			error = 0;
@@ -204,6 +204,7 @@ static ssize_t state_store(struct kobject *kobj, struct kobj_attribute *attr,
 #else
 		error = enter_state(state);
 #endif
+	}
 #endif
 
  Exit:
