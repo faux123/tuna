a56c8a3fb0a1b45116392f36f5278a1076049b2f
faux123 <reioux@gmail.com>
2012-02-17 00:00:39 -0800

    memblock, x86: Make ARCH_DISCARD_MEMBLOCK a config option
    From 6839454ae63f1eb21e515c10229ca95c22955fec Mon Sep 17 00:00:00 2001
    From: Tejun Heo <tj@kernel.org>
    Date: Thu, 14 Jul 2011 11:22:17 +0200
    
    Make ARCH_DISCARD_MEMBLOCK a config option so that it can be handled
    together with other MEMBLOCK options.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Link: http://lkml.kernel.org/r/20110714094603.GH3455@htj.dyndns.org
    Cc: Yinghai Lu <yinghai@kernel.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    
    Conflicts:
    
    	arch/x86/Kconfig
    	arch/x86/include/asm/memblock.h
    	include/linux/memblock.h

--- arch/x86/include/asm/memblock.h
+++ arch/x86/include/asm/memblock.h
@@ -1,8 +1,6 @@
 #ifndef _X86_MEMBLOCK_H
 #define _X86_MEMBLOCK_H
 
-#define ARCH_DISCARD_MEMBLOCK
-
 u64 memblock_x86_find_in_range_size(u64 start, u64 *sizep, u64 align);
 
 void memblock_x86_reserve_range(u64 start, u64 end, char *name);
