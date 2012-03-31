ad4ea5ba01abd919cea33e025b8d00ac9c6d3a5b
faux123 <reioux@gmail.com>
2012-03-31 12:07:09 -0700

    Revert "serial: omap: disable serial port irq during suspend"
    This reverts commit a752602b54042fc100d8bc47b4022ad6649cf07b.

--- drivers/tty/serial/omap-serial.c
+++ drivers/tty/serial/omap-serial.c
@@ -576,8 +576,15 @@ static int serial_omap_startup(struct uart_port *port)
 {
 	struct uart_omap_port *up = (struct uart_omap_port *)port;
 	unsigned long flags = 0;
+	int retval;
 
-	enable_irq(up->port.irq);
+	/*
+	 * Allocate the IRQ
+	 */
+	retval = request_irq(up->port.irq, serial_omap_irq, up->port.irqflags,
+				up->name, up);
+	if (retval)
+		return retval;
 
 	dev_dbg(up->port.dev, "serial_omap_startup+%d\n", up->pdev->id);
 
@@ -686,7 +693,7 @@ static void serial_omap_shutdown(struct uart_port *port)
 		up->uart_dma.rx_buf = NULL;
 	}
 	serial_omap_port_disable(up);
-	disable_irq(up->port.irq);
+	free_irq(up->port.irq, up);
 }
 
 static inline void
@@ -1258,7 +1265,6 @@ static int serial_omap_suspend(struct device *dev)
 	struct uart_omap_port *up = dev_get_drvdata(dev);
 
 	if (up) {
-		disable_irq(up->port.irq);
 		if (up->rts_mux_driver_control) {
 			up->rts_pullup_in_suspend = 1;
 			omap_rts_mux_write(MUX_PULL_UP, up->port.line);
@@ -1277,7 +1283,6 @@ static int serial_omap_resume(struct device *dev)
 	if (up) {
 		uart_resume_port(&serial_omap_reg, &up->port);
 		up->suspended = false;
-		enable_irq(up->port.irq);
 	}
 
 	return 0;
@@ -1455,13 +1460,13 @@ static int serial_omap_probe(struct platform_device *pdev)
 	dma_rx = platform_get_resource_byname(pdev, IORESOURCE_DMA, "rx");
 	if (!dma_rx) {
 		ret = -EINVAL;
-		goto do_release_region;
+		goto err;
 	}
 
 	dma_tx = platform_get_resource_byname(pdev, IORESOURCE_DMA, "tx");
 	if (!dma_tx) {
 		ret = -EINVAL;
-		goto do_release_region;
+		goto err;
 	}
 
 	up = kzalloc(sizeof(*up), GFP_KERNEL);
@@ -1487,7 +1492,7 @@ static int serial_omap_probe(struct platform_device *pdev)
 	if (!up->port.membase) {
 		dev_err(&pdev->dev, "can't ioremap UART\n");
 		ret = -ENOMEM;
-		goto do_free;
+		goto err1;
 	}
 
 	up->port.flags = omap_up_info->flags;
@@ -1532,31 +1537,21 @@ static int serial_omap_probe(struct platform_device *pdev)
 	ui[pdev->id] = up;
 	serial_omap_add_console_port(up);
 
-	ret = request_irq(up->port.irq, serial_omap_irq, up->port.irqflags,
-				up->name, up);
-	if (ret)
-		goto do_iounmap;
-	disable_irq(up->port.irq);
-
 	ret = uart_add_one_port(&serial_omap_reg, &up->port);
 	if (ret != 0)
-		goto do_free_irq;
+		goto err1;
 
 	dev_set_drvdata(&pdev->dev, up);
 	platform_set_drvdata(pdev, up);
 
 	return 0;
-
-do_free_irq:
-	free_irq(up->port.irq, up);
-do_iounmap:
-	iounmap(up->port.membase);
-do_free:
+err:
+	dev_err(&pdev->dev, "[UART%d]: failure [%s]: %d\n",
+				pdev->id, __func__, ret);
+err1:
 	kfree(up);
 do_release_region:
 	release_mem_region(mem->start, (mem->end - mem->start) + 1);
-	dev_err(&pdev->dev, "[UART%d]: failure [%s]: %d\n",
-				pdev->id, __func__, ret);
 	return ret;
 }
 
@@ -1567,9 +1562,7 @@ static int serial_omap_remove(struct platform_device *dev)
 	platform_set_drvdata(dev, NULL);
 	if (up) {
 		pm_runtime_disable(&up->pdev->dev);
-		free_irq(up->port.irq, up);
 		uart_remove_one_port(&serial_omap_reg, &up->port);
-		iounmap(up->port.membase);
 		kfree(up);
 	}
 	return 0;
