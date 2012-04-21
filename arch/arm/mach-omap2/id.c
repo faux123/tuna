78537902633b145353d84259e71664b3316b082c
faux123 <reioux@gmail.com>
2012-04-21 10:16:22 -0700

    OMAP4: ID: Add detection of 4470 1.5Ghz silicon & IVA OPPNT and OPPSB
    STD_FUSE_PROD_ID_1[17:16] fuse determines silicon type:
    
    0x1 - Standard performance (1.3 GHz)
    0x2 - High performance (1.5 GHz)
    
    Some 4460 and 4470 devices support IVA OPPNT and OPPSB.
    
    Add flags to identify this feature.

--- arch/arm/mach-omap2/id.c
+++ arch/arm/mach-omap2/id.c
@@ -241,9 +241,10 @@ static void __init omap4_check_features(void)
 		/* High performance device */
 		if (cpu_is_omap443x())
 			omap4_features |= OMAP4_HAS_MPU_1_2GHZ;
-		else if (cpu_is_omap446x() || cpu_is_omap447x())
+		else if (cpu_is_omap446x() || cpu_is_omap447x()) {
 			omap4_features |= OMAP4_HAS_MPU_1_5GHZ;
-
+			omap4_features |= OMAP4_HAS_IVA_500MHZ;
+		}
 		/* Fall through to Standard device features */
 	case OMAP4_SILICON_TYPE_STANDARD:
 	default:
@@ -255,6 +256,10 @@ static void __init omap4_check_features(void)
 #ifdef CONFIG_CPU_OVERCLOCK
 			omap4_features |= OMAP4_HAS_MPU_1_5GHZ;
 #endif
+			omap4_features |= OMAP4_HAS_IVA_430MHZ;
+		} else if (cpu_is_omap447x()) {
+			omap4_features |= OMAP4_HAS_MPU_1_3GHZ;
+			omap4_features |= OMAP4_HAS_IVA_430MHZ;
 		}
 	}
 }
