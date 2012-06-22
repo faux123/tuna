7da3ce5f939cab99b043fd24cc6e813562b1ce4b
faux123 <reioux@gmail.com>
2012-06-22 15:35:15 -0700

    mm, oom: fix potential killing of thread that is disabled from oom killing
    /proc/sys/vm/oom_kill_allocating_task will immediately kill current when
    the oom killer is called to avoid a potentially expensive tasklist scan
    for large systems.
    
    Currently, however, it is not checking current's oom_score_adj value
    which may be OOM_SCORE_ADJ_MIN, meaning that it has been disabled from
    oom killing.
    
    This patch avoids killing current in such a condition and simply falls
    back to the tasklist scan since memory still needs to be freed.
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    modified for TUNA kernel from Linux 3.5+ by faux123

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -743,9 +743,9 @@ void out_of_memory(struct zonelist *zonelist, gfp_t gfp_mask,
 	check_panic_on_oom(constraint, gfp_mask, order, mpol_mask);
 
 	read_lock(&tasklist_lock);
-	if (sysctl_oom_kill_allocating_task &&
+	if (sysctl_oom_kill_allocating_task && current->mm &&
 	    !oom_unkillable_task(current, NULL, nodemask) &&
-	    current->mm) {
+	    current->signal->oom_score_adj != OOM_SCORE_ADJ_MIN) {
 		/*
 		 * oom_kill_process() needs tasklist_lock held.  If it returns
 		 * non-zero, current could not be killed so we must fallback to
