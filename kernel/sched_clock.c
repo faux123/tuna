932f37c6b201c4fac65c7dd7d7e3666598e6cd94
Peter Zijlstra <a.p.zijlstra@chello.nl>
2012-06-26 10:11:56 -0700

    sched: clock wrap bug in 2.6.35-stable kills scheduling
    On Mon, 2012-06-25 at 11:45 +0200, Peter Zijlstra wrote:
    > On Sun, 2012-06-24 at 19:32 +0200, Thomas Lange wrote:
    > > Bug was introduced in 2.6.35.12 and is still present in linux-2.6.35.y HEAD.
    >
    > Ok, so nobody cares about that.. what does something recent like 3.5-rc4
    > do?
    >
    > If that's fixed, find the patch that fixes it. If not, we'll have a
    > look.
    
    If anything, I think something like the below ought to cure things.

--- kernel/sched_clock.c
+++ kernel/sched_clock.c
@@ -74,7 +74,7 @@
  */
 unsigned long long __attribute__((weak)) sched_clock(void)
 {
-	return (unsigned long long)(jiffies - INITIAL_JIFFIES)
+	return (unsigned long long)(get_jiffies_64() - INITIAL_JIFFIES)
 					* (NSEC_PER_SEC / HZ);
 }
 EXPORT_SYMBOL_GPL(sched_clock);
