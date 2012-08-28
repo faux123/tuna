f1b5ca43f550543ab05729a5dede3a842541ba41
faux123 <reioux@gmail.com>
2012-08-27 20:43:42 -0500

    time: RCU permitted to stop idle entry via softirq
    RCU needs to be able to use softirq to stop idle entry in order to
    be able to drain RCU callbacks from the current CPU, which in turn
    enables faster entry into dyntick-idle mode, which in turn reduces power
    consumption.  This commit therefore silences the error message that is
    sometimes produced when the going-idle CPU suddenly finds that it has
    an RCU_SOFTIRQ to process.
    
    Signed-off-by: Paul E. McKenney <paul.mckenney@linaro.org>
    backported by faux123 from Linux 3.5+ to Linux 3.0

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -25,6 +25,8 @@
 
 #include "tick-internal.h"
 
+const int softirq_stop_idle_mask = (~(1 << RCU_SOFTIRQ));
+
 /*
  * Per cpu nohz control structure
  */
@@ -332,7 +334,8 @@ void tick_nohz_stop_sched_tick(int inidle)
 	if (unlikely(local_softirq_pending() && cpu_online(cpu))) {
 		static int ratelimit;
 
-		if (ratelimit < 10) {
+		if (ratelimit < 10 &&
+		    (local_softirq_pending() & softirq_stop_idle_mask)) {
 			printk(KERN_ERR "NOHZ: local_softirq_pending %02x\n",
 			       (unsigned int) local_softirq_pending());
 			ratelimit++;
