9b16693f75a639e4f8bf3356a7ec451da3d36799
faux123 <reioux@gmail.com>
2012-10-08 15:25:51 -0500

    Revert "sched: introduce temporary FAIR_GROUP_SCHED dependency for load-tracking"
    This reverts commit 3f185cef8ae9a610f0308e577133af9c5ca60784.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1206,13 +1206,7 @@ struct sched_entity {
 	/* rq "owned" by this entity/group: */
 	struct cfs_rq		*my_q;
 #endif
-/*
- * Load-tracking only depends on SMP, FAIR_GROUP_SCHED dependency below may be
- * removed when useful for applications beyond shares distribution (e.g.
- * load-balance).
- */
-#if defined(CONFIG_SMP) && defined(CONFIG_FAIR_GROUP_SCHED)
-	/* Per-entity load-tracking */
+#ifdef CONFIG_SMP
 	struct sched_avg	avg;
 #endif
 };
